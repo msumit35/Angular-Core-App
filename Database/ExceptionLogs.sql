c23ff0462650b08b858534960e02065661ede602
msumit35 <msumit35@gmail.com>
2020-03-21 14:16:30 +0530

    Database project added

---
+++ Database/ExceptionLogs.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ExceptionLogs](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Controller] [nvarchar](50) NOT NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[StackTrace] [nvarchar](max) NULL,
+	[LogTime] [datetimeoffset](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Database/Users.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Users](
+	[Id] [uniqueidentifier] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Username] [nvarchar](50) NOT NULL,
+	[EmailId] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](max) NOT NULL,
+	[CreatedOn] [datetimeoffset](7) NOT NULL,
+	[LastUpdatedOn] [datetimeoffset](7) NULL,
+	[RemovedOn] [datetimeoffset](7) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
