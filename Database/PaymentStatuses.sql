34c5fd15174d3913f4c6d42d48c64cb449d64d3b
msumit35 <msumit35@gmail.com>
2020-03-23 01:32:23 +0530

    Description column added

--- Database/PaymentStatuses.sql
+++ Database/PaymentStatuses.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[PaymentStatuses](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](20) NOT NULL,
- CONSTRAINT [PK_PaymentStatuses] PRIMARY KEY CLUSTERED 
+ [Description] NVARCHAR(100) NULL, 
+    CONSTRAINT [PK_PaymentStatuses] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
--- Database/ServiceProviders.sql
+++ Database/ServiceProviders.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[ServiceProviders](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
- CONSTRAINT [PK_ServiceProviders] PRIMARY KEY CLUSTERED 
+ [Description] NVARCHAR(100) NULL, 
+    CONSTRAINT [PK_ServiceProviders] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
